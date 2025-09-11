49e65cfc2ce973f170d87ec8a7ea291350133464
hemel <smhemel.eu@gmail.com>
2025-09-12 01:15:54 +0600

    Initial project set up
    This is a starter project. We have installed some required packages and added assets.

---
+++ src/app/layout.tsx
@@ -0,0 +1,24 @@
+import type { Metadata } from "next";
+import { Outfit } from "next/font/google";
+import "./globals.css";
+
+const outfit = Outfit({
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "S M HEMEL",
+  description: "Portfolio of S M Hemel",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${outfit.className} antialiased`}>{children}</body>
+    </html>
+  );
+}
