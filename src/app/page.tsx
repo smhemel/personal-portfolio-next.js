a38e01cd0094fe176fb544919db2b614b1d06715
hemel <smhemel.eu@gmail.com>
2025-09-14 03:24:54 +0600

    Complete Pricing Plans and Contact section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,10 @@ import Hero from '@/components/Hero';
 import About from '@/components/About';
 import Skills from '@/components/Skills';
 import Reviews from '@/components/Reviews';
+import Contact from '@/components/Contact';
 import Projects from '@/components/Projects';
 import Experience from '@/components/Experience';
+import PricingPlans from '@/components/PricingPlans';
 
 export default function Home() {
   return (
@@ -14,6 +16,8 @@ export default function Home() {
       <Skills />
       <Reviews />
       <Projects />
+      <PricingPlans />
+      <Contact />
     </div>
   );
 }
--- src/assets/index.js
+++ src/assets/index.js
@@ -304,3 +304,56 @@ export const projectsButton = [
   'FramerMotion',
   'ThreeJS',
 ]
+
+// Pricing Plans
+import CheckLineIcon from 'remixicon-react/CheckLineIcon'
+export const checkIcon = <CheckLineIcon />
+
+export const pricingPlans = [
+  {
+    title: 'Basic',
+    pricing: '$500 - $1,000',
+    features: [
+      'Up to 5 pages',
+      'Responsive design ',
+      'Basic SEO ',
+      'Contact form',
+      'Social media links',
+      '1 month support',
+    ],
+    recommended: 'Small businesses, personal websites, bloggers',
+  },
+  {
+    title: 'Premium',
+    pricing: '$5,000 - $10,000',
+    features: [
+      'Unlimited pages',
+      'Responsive design',
+      'Comprehensive SEO',
+      'Contact form ',
+      'Social media links',
+      'Advanced security',
+      'E-commerce (unlimited products)',
+      'Blog setup',
+      'Google Analytics with custom reports',
+      '6 months support',
+    ],
+    recommended: 'Medium-sized businesses, online stores, service providers',
+  },
+  {
+    title: 'Standard',
+    pricing: '$1,500 - $3,000',
+    features: [
+      'Up to 10 pages',
+      'Responsive design',
+      'Advanced SEO',
+      'CContact form',
+      'Social media links',
+      'E-commerce (20 products)',
+      'Blog setup',
+      'Google Analytics',
+      '3 months support',
+    ],
+    recommended: 'Large businesses, complex e-commerce sites, custom web applications',
+  },
+]
