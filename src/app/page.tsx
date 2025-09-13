1e1817e0098b23503c7ac6d7782b5214352d73ff
hemel <smhemel.eu@gmail.com>
2025-09-14 03:13:33 +0600

    Complete design of Reviews section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Hero from '@/components/Hero';
 import About from '@/components/About';
 import Skills from '@/components/Skills';
+import Reviews from '@/components/Reviews';
 import Experience from '@/components/Experience';
 
 export default function Home() {
@@ -10,6 +11,7 @@ export default function Home() {
       <About />
       <Experience />
       <Skills />
+      <Reviews />
     </div>
   );
 }
--- src/assets/index.js
+++ src/assets/index.js
@@ -178,3 +178,51 @@ export const skillsData = [
     icon: '/skills/github.png',
   },
 ]
+
+// Reviews
+import StarFillIcon from 'remixicon-react/StarFillIcon'
+import StarHalfLineIcon from 'remixicon-react/StarHalfLineIcon'
+import ArrowLeftSLineIcon from 'remixicon-react/ArrowLeftSLineIcon'
+import ArrowRightSLineIcon from 'remixicon-react/ArrowRightSLineIcon'
+
+/* eslint-disable react/jsx-key */
+export const starIcons = [<StarFillIcon />, <StarHalfLineIcon />]
+export const arrowIcons = [<ArrowLeftSLineIcon />, <ArrowRightSLineIcon />]
+
+export const reviewsData = [
+  {
+    image: '/reviews/client-4.png',
+    name: 'Mark T., Freelance Graphic Designer',
+    comment:
+      "Your work on our website has been phenomenal. The design is visually stunning and incredibly user-friendly. We've received countless compliments from our customers, and our sales have noticeably increased. Thank you for capturing our brand's essence so perfectly!",
+    stars: [1, 1, 1, 1, 0.5],
+  },
+  {
+    image: '/reviews/client-2.png',
+    name: 'Sarah B., E-commerce Store Owner',
+    comment:
+      "Working with you, has been an absolute pleasure. The portfolio website you created for me is sleek, modern, and beautifully showcases my work. Your attention to detail and creative flair are truly impressive. I couldn't be happier with the final result!",
+    stars: [1, 1, 1, 1, 1],
+  },
+  {
+    image: '/reviews/client-3.png',
+    name: 'Emily R., CEO of Tech Startup',
+    comment:
+      "The redesign of our corporate website exceeded all expectations. Your innovative ideas and professional execution have given us a site that is not only visually appealing but also highly functional. We've seen a significant improvement in user engagement thanks to your expertise.",
+    stars: [1, 1, 1, 1, 1],
+  },
+  {
+    image: '/reviews/client-1.png',
+    name: 'Bob W., Lifestyle Blogger',
+    comment:
+      "I absolutely love the blog design you created for me, [Web Designer's Name]. It's exactly what I envisioned—stylish, unique, and easy to navigate. Your responsiveness and creative input were invaluable. My readers are raving about the new look, and I couldn't be more thrilled!",
+    stars: [1, 1, 1, 1, 0.5],
+  },
+  {
+    image: '/reviews/client-5.png',
+    name: 'David H., Director of Non-Profit Organization',
+    comment:
+      "your work on our non-profit’s website has been transformative. The design is clean, engaging, and effectively conveys our mission. We've seen an increase in online donations and volunteer sign-ups, all thanks to your fantastic design. Your dedication and talent are greatly appreciated!",
+    stars: [1, 1, 1, 1, 1],
+  },
+]
